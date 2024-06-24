bfce05aea7df8c2794056da168e2bfd81e45baf2
Leonardo Camargo <leonardo_camargo31@hotmail.com>
2024-06-24 10:16:27 -0300

    chore: add express

---
+++ src/infrastructure/api/__tests__/customer.e2e.spec.ts
@@ -0,0 +1,35 @@
+import { app, sequelize } from '../express'
+import request from 'supertest'
+
+describe('E2E test for customer', () => {
+  beforeEach(async () => {
+    await sequelize.sync({ force: true })
+  })
+
+  afterAll(async () => {
+    await sequelize.close()
+  })
+
+  it('should create a customer', async () => {
+    const response = await request(app)
+      .post('/customers')
+      .send({
+        name: 'john',
+        address: {
+          street: 'Street 1',
+          number: 123,
+          zip: '12345-678',
+          city: 'São Paulo',
+          state: 'SP'
+        }
+      })
+
+    expect(response.status).toBe(200)
+    expect(response.body.name).toBe('john')
+    expect(response.body.address.street).toBe('john')
+    expect(response.body.address.number).toBe(123)
+    expect(response.body.address.zip).toBe('12345-678')
+    expect(response.body.address.city).toBe('São Paulo')
+    expect(response.body.address.state).toBe('SP')
+  })
+})
