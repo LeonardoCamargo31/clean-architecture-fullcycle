6586880ee75e38115d43c0542ef460842b8881b9
Leonardo Camargo <leonardo_camargo31@hotmail.com>
2024-06-28 16:41:11 -0300

    chore: add yup

---
+++ src/domain/customer/validator/customer.yup.validator.ts
@@ -0,0 +1,30 @@
+import { ValidatorInterface } from '../../@shared/validator/validator.interface'
+import { Customer } from '../entity/customer'
+import * as yup from 'yup'
+
+export class CustomerYupValidator implements ValidatorInterface<Customer> {
+  validate (entity: Customer): void {
+    try {
+      yup
+        .object()
+        .shape({
+          id: yup.string().required('id is required'),
+          name: yup.string().required('name is required')
+        })
+        .validateSync({
+          id: entity.id,
+          name: entity.name
+        }, {
+          abortEarly: false
+        })
+    } catch (errors) {
+      const err = errors as yup.ValidationError
+      err.errors.forEach((error) => {
+        entity.notification.addError({
+          context: 'customer',
+          message: error
+        })
+      })
+    }
+  }
+}
